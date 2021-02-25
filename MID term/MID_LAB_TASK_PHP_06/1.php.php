c4e532ae8367b625a4d69e397484fcd6ac4b3a6d
Monowarhossain-joy <hossainjoy512@gmail.com>
2021-02-25 23:30:32 +0600

    Login update

---
+++ MID term/MID_LAB_TASK_PHP_06/1.php.php
@@ -0,0 +1,35 @@
+<?php
+
+	if(isset($_POST['Submit']))
+	{
+
+		
+		$cur_pass	=  $_POST['current_pass'];
+		$new_pass  =  $_POST['new_pass'];
+
+		if($cur_pass == $new_pass)
+		{
+			echo "The new pass is the same as current pass";
+		}else{
+			echo "The pass didn't match Re-typing mistake";
+		}
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Password Change</title>
+</head>
+<body>
+	<form method="POST" action="">
+		<fieldset>
+			<legend>Password Change</legend>
+			Current Password :	<input type="password" name="password" value=""> <br>                    
+			New Password : 	    <input type="password" name="cur_pass" value=""> <br>
+								<input type="submit" name="submit" value="Submit"> <br>
+						    	
+		</fieldset>
+	</form>
+</body>
+</html>
